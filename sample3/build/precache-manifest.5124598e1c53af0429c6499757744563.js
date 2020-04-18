8b889ea468888ba3318eac44f9b74963d924d46d
nivedps <60648993+nivedps@users.noreply.github.com>
2020-04-18 21:07:59 +0530

    Add files via upload

---
+++ sample3/build/precache-manifest.5124598e1c53af0429c6499757744563.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "541a1b582b68b018bd677b0c989d1b9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "016038251991a05f9f5f",
+    "url": "/static/css/main.6f2fde6b.chunk.css"
+  },
+  {
+    "revision": "5cf78a7d96fc7f1855f8",
+    "url": "/static/js/2.04e7ff50.chunk.js"
+  },
+  {
+    "revision": "df8be702f0cc9d299c0ff42ca03d3e40",
+    "url": "/static/js/2.04e7ff50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "016038251991a05f9f5f",
+    "url": "/static/js/main.c72ce908.chunk.js"
+  },
+  {
+    "revision": "1ce0b1e85fe7640f9567",
+    "url": "/static/js/runtime-main.1cd19790.js"
+  },
+  {
+    "revision": "348aeb8e09cba21219e6e056e1b217fb",
+    "url": "/static/media/clogo.348aeb8e.png"
+  },
+  {
+    "revision": "2f316a4b635c319f6ca3a234a4e32095",
+    "url": "/static/media/cover.2f316a4b.jpg"
+  },
+  {
+    "revision": "d464a78caa3b12ba97e2b7cdc5986437",
+    "url": "/static/media/title.d464a78c.jpg"
+  }
+]);
